a142471628ba92a286f1738d280a28657f0bd61c
David Dzineku <44753593+Bigthinz@users.noreply.github.com>
2023-04-26 14:03:03 +0000

    feat: add country code

--- src/components/donut-chart/donut-chart.tsx
+++ src/components/donut-chart/donut-chart.tsx
@@ -1,5 +1,6 @@
 import { Card, DonutChart, Title } from '@tremor/react';
 import axios from 'axios';
+import countryLookup from 'country-code-lookup';
 import { useEffect, useState } from 'react';
 import { FlagIcon } from 'react-flag-kit';
 
@@ -63,28 +64,82 @@ const DonutCharts = () => {
     { code: 'FR', name: 'France', percentage: 5 },
   ];
 
-  const valueFormatter = (value) => `${value}%`;
+  const topLocations = [
+    {
+      country: 'Nigeria',
+      count: 68,
+      percent: 34,
+    },
+    {
+      country: 'Germany',
+      count: 37,
+      percent: 19,
+    },
+    {
+      country: 'Ghana',
+      count: 50,
+      percent: 25,
+    },
+    {
+      country: 'Finland',
+      count: 40,
+      percent: 20,
+    },
+    {
+      country: 'United Kingdom',
+      count: 4,
+      percent: 2,
+    },
+  ];
 
+  const locationsWithCodes = topLocations.map((location) => {
+    const countryCode = countryLookup.byCountry(location.country)?.iso2;
+    return {
+      ...location,
+      code: countryCode,
+    };
+  });
+
+  const valueFormatter = (value) => `${value}`;
+  const colors = [
+    '#6B7280',
+    '#8B5CF6',
+    '#6366F1',
+    '#EC4899',
+    '#22D3EE',
+    '#F59E0B',
+  ];
   return (
     <Card className='max-w-lg'>
       <Title>Top Locations</Title>
       <div className={`grid ${isMobile ? 'grid-cols-1' : 'grid-cols-2'} gap-6`}>
         <div>
           <div className='mt-5 space-y-3'>
-            {locations.map((location, index) => (
+            {locationsWithCodes.map((location, index) => (
               <div key={index} className='flex items-center space-x-2'>
                 <FlagIcon code={location.code} size={20} />
-                <span>{location.name}</span>
-                <span>{valueFormatter(location.percentage)}</span>
+                <span>{location.country}</span>
+                <span>{valueFormatter(location.percent)}</span>
+                <span className='inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 '>
+                  <svg
+                    className='h-2 w-2'
+                    viewBox='0 0 6 6'
+                    fill={colors[index]}
+                    aria-hidden='true'
+                  >
+                    <circle cx={3} cy={3} r={3} />
+                  </svg>
+                </span>
               </div>
             ))}
           </div>
         </div>
         <div className='mt-6'>
           <DonutChart
-            data={cities}
-            category='sales'
-            index='name'
+            data={topLocations}
+            category='percent'
+            index='country'
+            showLabel={false}
             valueFormatter={valueFormatter}
             colors={['slate', 'violet', 'indigo', 'rose', 'cyan', 'amber']}
           />
